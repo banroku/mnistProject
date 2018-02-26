3db9e69980b40d69f50f26a4d4cd28d4e61d2366
banroku <sakai.fuji.jpn@gmail.com>
2018-02-27 04:58:02 +0900

    added learnByNN.m

---
+++ learnByNN.m
@@ -0,0 +1,47 @@
+% === Learning By Logistic regression ===
+J = 0;
+lambda = 10;
+iter = 010;
+
+% make new theta
+if initializeTheta
+    theta = zeros(K, size(Xtrain, 1) + 1);
+    theta = theta(:);
+end
+
+% %calculate initical J and theta
+% [J_train, grad] = costLogisticReg(Xtrain, Ytrain, theta, lambda);
+
+tic();
+% train Logstic regression
+theta = trainLogisticReg(Xtrain, Ytrain, theta, lambda, iter);
+trainingTime = toc();
+
+% continue using trained theta
+initializeTheta = false;
+
+% calculate correct rate
+Pre_train = predict(Xtrain, theta);
+Acc_train = calculateAccuracy(Ytrain, Pre_train);
+
+% calculate parameters of cross-valication set
+J_train = costLogisticReg(Xtrain, Ytrain, theta, 0);
+J_cv = costLogisticReg(Xcv, Ycv, theta, 0);
+Pre_cv= predict(Xcv, theta);
+Acc_cv = calculateAccuracy(Ycv, Pre_cv);
+
+fprintf('Training time is: %f \n', trainingTime);
+fprintf('Training time per iter: %f \n', trainingTime/iter);
+fprintf('Cost of train set is: %f \n', J_train);
+fprintf('Cost of cv set is: %f \n', J_cv);
+fprintf('Accuracy of train set is: %f \n', Acc_train);
+fprintf('Accuracy of cv set is: %f \n', Acc_cv);
+
+% calculate costs
+
+
+
+
+
+% === End: Logistic regression ===
+
